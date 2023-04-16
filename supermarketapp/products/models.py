99c526baaabb1daa3476e73d0a8f5ec237721dd1
Rafael Farfan| <rafaeldanielfarfan@gmail.com>
2023-04-16 11:20:21 -0500

    migrations

--- supermarketapp/products/models.py
+++ supermarketapp/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    
+    id= models.AutoField(primary_key=True)
+    precio = models.IntegerField(verbose_name='Precio')
+    nombre_producto = models.CharField(max_length=30, verbose_name='NombreProducto')
+
+
